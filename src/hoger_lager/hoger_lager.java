5428efbc63cd2e74d0316f810cd193593aecc866
Pieter Huijgens <pieterhuygens@msn.com>
2016-11-11 14:02:04 +0100

    Tweede versie.

--- src/hoger_lager/hoger_lager.java
+++ src/hoger_lager/hoger_lager.java
@@ -13,30 +13,46 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/hoger_lager")
 public class hoger_lager extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	String hint = " ";
 	Random rand = new Random();
 	Integer secret = rand.nextInt(100);
 	int count = 0;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		count = 0;
 		request.setAttribute("hint", hint);
 		request.setAttribute("count", count);
 		request.getRequestDispatcher("/WEB-INF/HogerLager.jsp").forward(request, response);
 
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Integer Gok = Integer.parseInt(request.getParameter("gok"));
-		if(Gok != null){
-		if(Gok<secret){hint = "Je moet hoger gokken.";}
-		if(secret<Gok){hint = "Je moet lager gokken.";}
-		if(secret == Gok){hint = "gefeliciteerd, je hebt het geraden. Voer een nieuw getal in om het nog een keer te spelen."; count = 0; secret = rand.nextInt(100);}}
-		count++;
+		if (hint.equals("gefeliciteerd, je hebt het geraden. Voer een nieuw getal in om het nog een keer te spelen.")) {
+			count = 0;
+		}
+		if (Gok != null) {
+			if (Gok < secret) {
+				hint = "Je moet hoger gokken.";
+				count++;
+			}
+			if (secret < Gok) {
+				hint = "Je moet lager gokken.";
+				count++;
+			}
+			if (secret == Gok) {
+				hint = "gefeliciteerd, je hebt het geraden. Voer een nieuw getal in om het nog een keer te spelen.";
+				//count = 0;
+				secret = rand.nextInt(100);
+			}
+		}
 		request.setAttribute("count", count);
 		request.setAttribute("hint", hint);
 		request.getRequestDispatcher("/WEB-INF/HogerLager.jsp").forward(request, response);
-		
-		
-}
+
+	}
 
 }
